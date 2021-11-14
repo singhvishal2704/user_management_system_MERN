8cde690050b3d3547127c3bbc67ea83247d6a194
Vishal Singh <singhvishal2704@gmail.com>
2021-11-15 00:04:58 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; ShunyEka</Col>
+          <Col className="text-center py-3">Copyright &copy; Project</Col>
         </Row>
       </Container>
     </footer>
